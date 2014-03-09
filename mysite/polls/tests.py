3109387d07940f8c2c2b5974914c6962e3c4e754
Ash Hoover <strixcuriosus@gmail.com>
2014-03-09 10:50:11 -0700

    add poll index detail tests class

--- mysite/polls/tests.py
+++ mysite/polls/tests.py
@@ -15,6 +15,18 @@ def create_poll(question, days):
     return Poll.objects.create(question=question,
         pub_date=timezone.now() + datetime.timedelta(days=days))
 
+class PollIndexDetailTests(TestCase):
+    def test_detail_view_with_a_future_poll(self):
+        """
+        The detail of a poll with a pub_date in the future
+        should return a 404 not found.
+        """
+        future_poll = create_poll(question='Future poll.', days=5)
+        response = self.client.get(reverse('polls:detail', args=(future_poll.id,)))
+        self.assertEqual(response.status_code, 404)
+
+    
+
 class PollViewTests(TestCase):
     def test_index_view_with_no_polls(self):
         """
@@ -43,6 +55,30 @@ class PollViewTests(TestCase):
         response = self.client.get(reverse('polls:index'))
         self.assertContains(response, "No polls are available.", status_code=200)
         self.assertQuerysetEqual(response.context['latest_poll_list'],[])
+
+    def test_index_view_with_future_poll_and_past_poll(self):
+        """
+        Even if both past and future polls exist, only past polls should be displayed.
+        """
+        create_poll(question="Past poll.", days=-30)
+        create_poll(question="Future poll.", days=30)
+        response = self.client.get(reverse('polls:index'))
+        self.assertQuerysetEqual(
+            response.context['latest_poll_list'],
+            ['<Poll: Past poll.>']
+        )
+    def test_index_view_with_two_past_polls(self):
+        """
+        The polls index page may display multiple polls.
+        """
+        create_poll(question="Past poll 1.", days=-30)
+        create_poll(question="Past poll 2.", days=-5)
+        response = self.client.get(reverse('polls:index'))
+        self.assertQuerysetEqual(
+            response.context['latest_poll_list'],
+            ['<Poll:Past poll 2.>', '<Poll: Past poll 1.>']
+        )
+
 
 class PollMethodTests(TestCase):
 
